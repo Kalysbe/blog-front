5c58c323ed69106ad0165422eca74d4aca5c3b2c
Kalysbe <42235644+Kalysbe@users.noreply.github.com>
2023-10-23 00:41:33 +0600

    first commit 2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instanse = axios.create({
-    baseURL: 'http://localhost:4444'
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 instanse.interceptors.request.use((config) => {
