2d924c9de449589e11a2c64aab4b2fee62ed6c8e
Kalysbe <42235644+Kalysbe@users.noreply.github.com>
2023-10-23 00:35:43 +0600

    first commit

---
+++ src/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const instanse = axios.create({
+    baseURL: 'http://localhost:4444'
+})
+
+instanse.interceptors.request.use((config) => {
+ config.headers.Authorization = window.localStorage.getItem('token')
+ return config
+})
+
+
+// axios.get('')
+
+export default instanse
